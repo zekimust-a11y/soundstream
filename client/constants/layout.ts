52947f50e159e404b6f69bd5f1c8b6c0d80c87f9
zekimust-a11y <zekimust@gmail.com>
2026-01-03 07:46:55 +0000

    Client: add desktop layout constants

---
+++ client/constants/layout.ts
@@ -0,0 +1,9 @@
+// Layout constants shared across desktop/web screens.
+
+// `DesktopSidebar` is fixed width on large web layouts.
+// If screens size their grids using full `windowWidth` instead of available content width,
+// the rightmost column can get clipped.
+export const DESKTOP_SIDEBAR_WIDTH = 320;
+
+
+
